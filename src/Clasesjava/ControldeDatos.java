fdbd396c18ffb079b15480afdeec4bedc7dc0487
r2d203 <112904387+r2d203@users.noreply.github.com>
2024-04-09 15:43:04 +0200

    ControldeDatos.java

--- src/Clasesjava/ControldeDatos.java
+++ src/Clasesjava/ControldeDatos.java
@@ -2,4 +2,39 @@ package Clasesjava;
 
 public class ControldeDatos {
 
+		public void iniciarPartida() {
+			
+		}
+		
+		public void iniciarPartidaGuardada(String id) {
+			
+		}
+		
+		public void gestionarTurno() {
+			
+		}
+		
+		public void gestionarVacuna() {
+			
+		}
+		
+		public void gestionarInfeccion() {
+			
+		}
+		
+		public void gestionarBrote() {
+			
+		}
+		
+		public void gestionarFinpartida() {
+			
+		}
+		
+		public void gestionarFrases() {
+			
+		}
+		
+		public void gestionarCura() {
+			
+		}
 }
