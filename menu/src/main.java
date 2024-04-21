9f9261d53d3fda0b8e2e2ef6e783e466e92ddfc0
r2d203 <112904387+r2d203@users.noreply.github.com>
2024-04-21 22:40:24 +0200

    Add files via upload

---
+++ menu/src/main.java
@@ -0,0 +1,15 @@
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class main {
+
+	public static void main(String[] args) {
+		marco menu = new marco();
+		//imgFondo i = new imgFondo();
+
+
+
+	}
+
+}
